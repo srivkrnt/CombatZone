1f59f0aba279fe1ed95c2d3c887aa952b6561220
srivkrnt <sri.vkrnt@gmail.com>
2018-05-27 22:31:33 +0530

    Pushing files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Homepage from './models/Homepage';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div> 
+            <Homepage/>
+      </div>
+    );
+  }
+}
+
+export default App;
